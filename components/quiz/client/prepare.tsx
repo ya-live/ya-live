e136ff7eff0e9095d75609e20a68a005d690963b
SO JUNG <0901sj@gmail.com>
2019-12-19 14:29:59 +0900

    💫 클라이언트 준비화면 join api 적용

--- components/quiz/client/prepare.tsx
+++ components/quiz/client/prepare.tsx
@@ -1,16 +1,55 @@
 import { Button } from 'antd';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { DateTime } from 'luxon';
 import { useAuth } from '@/components/auth/hooks/auth_hooks';
 
 import { QuizClientContext } from '../../../context/quiz/client/QuizClientContext';
 import FirebaseAuthClient from '../../../models/commons/firebase_auth_client.model';
+import * as participantClient from '../../../models/quiz/participants.client.service';
 import styles from './body.css';
 
 const Prepare: React.FC = () => {
-  const { user: userInfo, haveUser } = useAuth();
+  const { user } = useAuth();
   const ctx = useContext(QuizClientContext);
-  const email = userInfo?.email || '';
+  const email = user?.email || '';
 
+  useEffect(() => {
+    // eslint-disable-next-line no-unused-expressions
+    (async () => {
+      if (!user) {
+        // eslint-disable-next-line no-alert
+        alert('잘못된 접근입니다. 다시 로그인해주세요.');
+        onClickSignOut();
+
+        return;
+      }
+
+      const resp = await participantClient.joinParticipantsForClient({
+        uid: user.uid,
+        quiz_id: ctx.quizID,
+        isServer: false,
+        info: {
+          id: ctx.quizID,
+          join: DateTime.local().toISO(),
+          alive: true,
+          displayName: user.displayName === null ? 'empty' : user.displayName,
+        },
+      });
+
+      if (
+        resp.status === 401 ||
+        (ctx.quiz?.possibleEmailAddress &&
+          ctx.quiz?.possibleEmailAddress !== user.email?.split('@')[1])
+      ) {
+        // eslint-disable-next-line no-alert
+        alert('귀하의 이메일 계정으로는 참가할 수 없는 퀴즈입니다.');
+        onClickSignOut();
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   async function onClickSignOut() {
     await FirebaseAuthClient.getInstance().Auth.signOut();
     window.location.href = `/quiz/${ctx.quizID}`;
@@ -32,9 +71,7 @@ const Prepare: React.FC = () => {
     </>
   );
 
-  return (
-    <section className={styles.container}>{haveUser ? prepareEle : '잘못된 접근입니다.'}</section>
-  );
+  return <section className={styles.container}>{prepareEle}</section>;
 };
 
 export default Prepare;
